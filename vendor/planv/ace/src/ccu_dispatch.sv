3f2f9ce5fd973c597470ac3273152349e351ef32
Luca Valente <luca.valente2@unibo.it>
2024-04-09 15:08:25 +0200

    `ccu_dispatch`: check cacheline accesses, not just elements.
     * Check cacheline accesses, not just elements.
     * Fix `infligth_trx_d.read/write.valid` assign

--- vendor/planv/ace/src/ccu_dispatch.sv
+++ vendor/planv/ace/src/ccu_dispatch.sv
@@ -17,6 +17,7 @@
 
 module ccu_dispatch
  import axi_pkg::*;
+ import ariane_pkg::*;
  #(
   parameter int  NoPorts = '0,
   parameter int  AxiAddrWidth = 64,
@@ -30,6 +31,8 @@ module ccu_dispatch
    output req_t  [NoPorts-1:0]  ccu_req_o,
    input  resp_t [NoPorts-1:0]  ccu_resp_i
 );
+
+   localparam int unsigned DCacheByteOffset = $clog2(ariane_pkg::DCACHE_LINE_WIDTH/8);
 
    typedef struct packed {
       logic                    valid;
@@ -48,43 +51,51 @@ module ccu_dispatch
       inflight_trx_d = inflight_trx_q;
       for (int i = 0; i < NoPorts; i++) begin : check_incoming_reqs
          logic [NoPorts-1:0] w_overlap;
+         logic [NoPorts-1:0] aw_valids;
          logic [NoPorts-1:0] r_overlap;
+         logic [NoPorts-1:0] ar_valids;
          ax_trx_t to_open_trx;
          w_overlap = '0;
+         aw_valids = '0;
          r_overlap = '0;
+         ar_valids = '0;
          to_open_trx = '0;
          ccu_req_o[i] = core_req_i[i];
          core_resp_o[i] = ccu_resp_i[i];
+         for(int k = 0; k < NoPorts ; k++) begin
+            aw_valids[k] = core_req_i[k].aw_valid & (k<i);
+            ar_valids[k] = core_req_i[k].ar_valid & (k<i);
+         end
          if(core_req_i[i].aw_valid) begin : aw_req
-            to_open_trx.write.start_addr = axi_pkg::aligned_addr(core_req_i[i].aw.addr,core_req_i[i].aw.size);
-            to_open_trx.write.end_addr = axi_pkg::aligned_addr(core_req_i[i].aw.addr,core_req_i[i].aw.size) + (axi_pkg::num_bytes(core_req_i[i].aw.size) * (core_req_i[i].aw.len + 1));
+            to_open_trx.write.start_addr = ( axi_pkg::aligned_addr(core_req_i[i].aw.addr,core_req_i[i].aw.size) >> DCacheByteOffset ) << DCacheByteOffset;
+            to_open_trx.write.end_addr = (( axi_pkg::aligned_addr(core_req_i[i].aw.addr,core_req_i[i].aw.size) + (axi_pkg::num_bytes(core_req_i[i].aw.size) * (core_req_i[i].aw.len + 1)) ) >> DCacheByteOffset ) << DCacheByteOffset;
             for (int j = 0; j < NoPorts ; j++) begin
                if ( i == j ) begin
-                  w_overlap[j] = 1'b0;
+                  w_overlap[j] = inflight_trx_q[j].read.valid;
                end else begin
                   if(inflight_trx_q[j].write.valid) begin
                      if( (to_open_trx.write.start_addr >= inflight_trx_q[j].write.start_addr) && (to_open_trx.write.end_addr <= inflight_trx_q[j].write.end_addr) )begin
                         w_overlap[j] = 1'b1;
-                     end else if( (to_open_trx.write.start_addr <= inflight_trx_q[j].write.end_addr) && (to_open_trx.write.end_addr > inflight_trx_q[j].write.end_addr)) begin
+                     end else if( (to_open_trx.write.start_addr <= inflight_trx_q[j].write.end_addr) && (to_open_trx.write.end_addr >= inflight_trx_q[j].write.end_addr)) begin
                         w_overlap[j] = 1'b1;
-                     end else if( (to_open_trx.write.end_addr >= inflight_trx_q[j].write.start_addr) && (to_open_trx.write.start_addr < inflight_trx_q[j].write.start_addr)) begin
+                     end else if( (to_open_trx.write.end_addr >= inflight_trx_q[j].write.start_addr) && (to_open_trx.write.start_addr <= inflight_trx_q[j].write.start_addr)) begin
                         w_overlap[j] = 1'b1;
                      end
                   end
                   if(inflight_trx_q[j].read.valid) begin
                      if( (to_open_trx.write.start_addr >= inflight_trx_q[j].read.start_addr) && (to_open_trx.write.end_addr <= inflight_trx_q[j].read.end_addr) )begin
                         w_overlap[j] = 1'b1;
-                     end else if( (to_open_trx.write.start_addr <= inflight_trx_q[j].read.end_addr) && (to_open_trx.write.end_addr > inflight_trx_q[j].read.end_addr)) begin
+                     end else if( (to_open_trx.write.start_addr <= inflight_trx_q[j].read.end_addr) && (to_open_trx.write.end_addr >= inflight_trx_q[j].read.end_addr)) begin
                         w_overlap[j] = 1'b1;
-                     end else if( (to_open_trx.write.end_addr >= inflight_trx_q[j].read.start_addr) && (to_open_trx.write.start_addr < inflight_trx_q[j].read.start_addr)) begin
+                     end else if( (to_open_trx.write.end_addr >= inflight_trx_q[j].read.start_addr) && (to_open_trx.write.start_addr <= inflight_trx_q[j].read.start_addr)) begin
                         w_overlap[j] = 1'b1;
                      end
                   end
                end // else: !if( i == j )
             end // for (genvar j = 0; j < NoPorts ; j++)
-            ccu_req_o[i].aw_valid = core_req_i[i].aw_valid & ~(|w_overlap);
+            ccu_req_o[i].aw_valid = core_req_i[i].aw_valid & ~(|w_overlap) & ~(|aw_valids) & ~(|ar_valids);
             core_resp_o[i].aw_ready = ccu_resp_i[i].aw_ready & ~(|w_overlap);
-            if(core_resp_o[i].aw_ready & core_req_i[i].aw_valid) begin
+            if(core_resp_o[i].aw_ready & ccu_req_o[i].aw_valid) begin
                inflight_trx_d[i].write = to_open_trx.write;
                inflight_trx_d[i].write.valid = 1'b1;
             end
@@ -93,35 +104,35 @@ module ccu_dispatch
             inflight_trx_d[i].write.valid = 1'b0;
          end
          if(core_req_i[i].ar_valid) begin : ar_req
-            to_open_trx.read.start_addr = axi_pkg::aligned_addr(core_req_i[i].ar.addr,core_req_i[i].aw.size);
-            to_open_trx.read.end_addr = axi_pkg::aligned_addr(core_req_i[i].ar.addr,core_req_i[i].aw.size) + (axi_pkg::num_bytes(core_req_i[i].ar.size) * (core_req_i[i].ar.len + 1));
+            to_open_trx.read.start_addr = ( axi_pkg::aligned_addr(core_req_i[i].ar.addr,core_req_i[i].ar.size) >> DCacheByteOffset ) << DCacheByteOffset;
+            to_open_trx.read.end_addr = (( axi_pkg::aligned_addr(core_req_i[i].ar.addr,core_req_i[i].ar.size) + (axi_pkg::num_bytes(core_req_i[i].ar.size) * (core_req_i[i].ar.len + 1)) ) >> DCacheByteOffset ) << DCacheByteOffset;
             for (int j = 0; j < NoPorts ; j++) begin
                if ( i == j ) begin
-                  r_overlap[j] = 1'b0;
+                  r_overlap[j] = inflight_trx_q[j].write.valid;
                end else begin
                   if(inflight_trx_q[j].write.valid) begin
                      if( (to_open_trx.read.start_addr >= inflight_trx_q[j].write.start_addr) && (to_open_trx.read.end_addr <= inflight_trx_q[j].write.end_addr) )begin
                         r_overlap[j] = 1'b1;
-                     end else if( (to_open_trx.read.start_addr <= inflight_trx_q[j].write.end_addr) && (to_open_trx.read.end_addr > inflight_trx_q[j].write.end_addr)) begin
+                     end else if( (to_open_trx.read.start_addr <= inflight_trx_q[j].write.end_addr) && (to_open_trx.read.end_addr >= inflight_trx_q[j].write.end_addr)) begin
                         r_overlap[j] = 1'b1;
-                     end else if( (to_open_trx.read.end_addr >= inflight_trx_q[j].write.start_addr) && (to_open_trx.read.start_addr < inflight_trx_q[j].write.start_addr)) begin
+                     end else if( (to_open_trx.read.end_addr >= inflight_trx_q[j].write.start_addr) && (to_open_trx.read.start_addr <= inflight_trx_q[j].write.start_addr)) begin
                         r_overlap[j] = 1'b1;
                      end
                   end
                   if(inflight_trx_q[j].read.valid) begin
                      if( (to_open_trx.read.start_addr >= inflight_trx_q[j].read.start_addr) && (to_open_trx.read.end_addr <= inflight_trx_q[j].read.end_addr) )begin
                         r_overlap[j] = 1'b1;
-                     end else if( (to_open_trx.read.start_addr <= inflight_trx_q[j].read.end_addr) && (to_open_trx.read.end_addr > inflight_trx_q[j].read.end_addr)) begin
+                     end else if( (to_open_trx.read.start_addr <= inflight_trx_q[j].read.end_addr) && (to_open_trx.read.end_addr >= inflight_trx_q[j].read.end_addr)) begin
                         r_overlap[j] = 1'b1;
-                     end else if( (to_open_trx.read.end_addr >= inflight_trx_q[j].read.start_addr) && (to_open_trx.read.start_addr < inflight_trx_q[j].read.start_addr)) begin
+                     end else if( (to_open_trx.read.end_addr >= inflight_trx_q[j].read.start_addr) && (to_open_trx.read.start_addr <= inflight_trx_q[j].read.start_addr)) begin
                         r_overlap[j] = 1'b1;
                      end
                   end
                end // else: !if( i == j )
             end // for (genvar j = 0; j < NoPorts ; j++)
-            ccu_req_o[i].ar_valid = core_req_i[i].ar_valid & ~(|r_overlap);
+            ccu_req_o[i].ar_valid = core_req_i[i].ar_valid & ~(|r_overlap) & ~(|aw_valids) & ~(|ar_valids);
             core_resp_o[i].ar_ready = ccu_resp_i[i].ar_ready & ~(|r_overlap);
-            if(core_resp_o[i].ar_ready & core_req_i[i].ar_valid) begin
+            if(core_resp_o[i].ar_ready & ccu_req_o[i].ar_valid) begin
                inflight_trx_d[i].read = to_open_trx.read;
                inflight_trx_d[i].read.valid = 1'b1;
             end
