486e7e0f413e26df4474ebfb9e827f1863821da4
Luca Valente <luca.valente2@unibo.it>
2024-04-11 16:24:26 +0200

    Solve `'mshr_d' might be read before written in always_comb or always @* block`

--- core/cache_subsystem/miss_handler.sv
+++ core/cache_subsystem/miss_handler.sv
@@ -187,6 +187,8 @@ module miss_handler import ariane_pkg::*; import std_cache_pkg::*; #(
     // ------------------------------
     // Cache Management
     // ------------------------------
+    assign miss_id_o = mshr_d.id; // to performance counter
+
     always_comb begin : cache_management
         automatic logic [DCACHE_SET_ASSOC-1:0] evict_way, valid_way;
 
@@ -234,7 +236,6 @@ module miss_handler import ariane_pkg::*; import std_cache_pkg::*; #(
         // core
         flush_ack_o         = 1'b0;
         miss_o              = 1'b0; // to performance counter
-        miss_id_o           = mshr_d.id; // to performance counter
         serve_amo_d         = serve_amo_q;
         // --------------------------------
         // Flush and Miss operation
