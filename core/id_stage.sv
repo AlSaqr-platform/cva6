1acca5b2fb13fead0e6c0ea4f6621ada19a05ed3
Maicol Ciani <maicol.ciani@unibo.it>
2024-12-24 01:27:09 +0100

    Revert "feat(smaia/ssaia): add RISC-V AIA core extensions"
    This reverts commit 5b01253bdcf7f166dcdf336b3516a4ebe8c467a0.

--- core/id_stage.sv
+++ core/id_stage.sv
@@ -55,7 +55,7 @@ module id_stage #(
     // Vector extension status - CSR_REGFILE
     input riscv::xs_t vs_i,
     // Level sensitive (async) interrupts - SUBSYSTEM
-    input logic [ariane_pkg::NrIntpFiles-1:0] irq_i,
+    input logic [1:0] irq_i,
     // Interrupt control status - CSR_REGFILE
     input ariane_pkg::irq_ctrl_t irq_ctrl_i,
     // TO_BE_COMPLETED - CLIC_CTRL
@@ -75,20 +75,7 @@ module id_stage #(
     // Trap sret - CSR_REGFILE
     input logic tsr_i,
     // Hypervisor user mode - CSR_REGFILE
-    input  logic hu_i,
-    output logic [riscv::XLEN-1:0] mtopi_o,
-    output logic [riscv::XLEN-1:0] stopi_o,
-    output logic [riscv::XLEN-1:0] vstopi_o,
-    // MENV Shadow Stack enable - CSR_REGFILE
-    input logic menv_sse_i,
-    // HENV Shadow Stack enable - CSR_REGFILE
-    input logic henv_sse_i,
-    // SENV Shadow Stack enable - CSR_REGFILE
-    input logic senv_sse_i,
-    // Shadow Stack enabled state - EX_STAGE
-    output logic xsse_o,
-    // Shadow stack test mode - CSR_REGFILE
-    input logic ss_testmode_i
+    input logic hu_i
 );
   // ID/ISSUE register stage
   typedef struct packed {
@@ -177,10 +164,7 @@ module id_stage #(
       .henv_sse_i,
       .instruction_o          (decoded_instruction),
       .orig_instr_o           (orig_instr),
-      .is_control_flow_instr_o(is_control_flow_instr),
-      .mtopi_o                (mtopi_o),
-      .stopi_o                (stopi_o),
-      .vstopi_o               (vstopi_o)
+      .is_control_flow_instr_o(is_control_flow_instr)
   );
 
   // ------------------
